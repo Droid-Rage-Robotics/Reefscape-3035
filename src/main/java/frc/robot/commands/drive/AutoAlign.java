ec3f0e222265f1fb7cd2da7e19d0189aa7412773
Sai Varshini Chinnasani <saivarshini.chinnasani@gmail.com>
2025-03-25 13:54:55 -0500

    Copied TeleopAlign to AutoAlign

--- src/main/java/frc/robot/commands/drive/AutoAlign.java
+++ src/main/java/frc/robot/commands/drive/AutoAlign.java
@@ -2,56 +2,77 @@ package frc.robot.commands.drive;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.DroidRageConstants;
 import frc.robot.subsystems.drive.SwerveDrive;
 import frc.robot.subsystems.vision.Vision;
 
-//Autonomous
 public class AutoAlign extends Command {
 	private SwerveDrive drive;
 	private Vision vision;
-	private PIDController rotController = new PIDController(.07, 0, 0);
-	private PIDController xController = new PIDController(.07, 0, 0);
 
+	// TELEOP
 	public AutoAlign(SwerveDrive drive, Vision vision) {
 		this.drive = drive;
 		this.vision = vision;
-		rotController.setTolerance(.3);
-		xController.setTolerance(.2);
-
-		// addRequirements(drive, vision);
+		addRequirements(drive, vision);
 	}
 
 	@Override
 	public void execute() {
-		drive.drive(limelight_range_proportional(), 0, limelight_aim_proportional());// - 0.03);
+		switch (DroidRageConstants.alignmentMode) {
+			case LEFT:
+				if (!vision.gettV(DroidRageConstants.leftLimelight)) {
+					return;
+				}
+				break;
+			case RIGHT:
+				if (!vision.gettV(DroidRageConstants.rightLimelight)) {
+					return;
+				}
+				break;
+		}
+		drive.drive(limelight_range_proportional(), 0, limelight_aim_proportional() - 0.03);
 	}
 
 	@Override
 	public boolean isFinished() {
 		// return !driver.povUp().getAsBoolean();
 		return (vision.rotController.atSetpoint() && vision.xController.atSetpoint());// || timer.hasElapsed(3);
+	}
 
+	double limelight_aim_proportional() {
+		double targetingAngularVelocity = 0;
+		switch (DroidRageConstants.alignmentMode) {
+			case LEFT:
+				targetingAngularVelocity = vision.rotController.calculate(
+						vision.gettX(DroidRageConstants.leftLimelight), Vision.Location.LEFT_L.getAngle());
+				break;
+			case RIGHT:
+				targetingAngularVelocity = vision.rotController.calculate(
+						vision.gettX(DroidRageConstants.rightLimelight), Vision.Location.RIGHT_R.getAngle());
+				break;
+		}
+		return targetingAngularVelocity;// -
 	}
 
 	double limelight_range_proportional() {
-		double targetingForwardSpeed = xController.calculate(
-			vision.gettY(DroidRageConstants.leftLimelight), Vision.Location.LEFT_L.getDistance());
+		double targetingForwardSpeed = 0;
+		switch (DroidRageConstants.alignmentMode) {
+			case LEFT:
+				targetingForwardSpeed = vision.xController.calculate(
+						vision.gettY(DroidRageConstants.leftLimelight), Vision.Location.LEFT_L.getDistance());
+				break;
+			case RIGHT:
+				targetingForwardSpeed = vision.xController.calculate(
+						vision.gettY(DroidRageConstants.rightLimelight), Vision.Location.RIGHT_R.getDistance());
+				break;
+		}
+
+		// = xController.calculate(vision.gettY(DroidRageConstants.leftLimelight), -2);
 		// targetingForwardSpeed *=
 		// SwerveDriveConstants.SwerveDriveConfig.MAX_SPEED_METERS_PER_SECOND.getValue();
 		return targetingForwardSpeed;
 	}
 
-	double limelight_aim_proportional() {
-		double targetingAngularVelocity = rotController.calculate(
-			vision.gettX(DroidRageConstants.leftLimelight), Vision.Location.LEFT_L.getAngle());
-		// targetingAngularVelocity *=
-		// SwerveDriveConstants.SwerveDriveConfig.MAX_ANGULAR_ACCELERATION_RADIANS_PER_SECOND_SQUARED.getValue();
-		return targetingAngularVelocity;// -
-	}
-
-	
-
 }
-
-		
