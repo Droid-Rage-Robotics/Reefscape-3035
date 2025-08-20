f27bf17178c9c405d8a5d58c2f117e007ea50773
ryancave282 <ryancueva282@gmail.com>
2025-08-20 12:49:07 -0500

    mixer testing

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -29,12 +29,12 @@ import frc.robot.subsystems.vision.Vision;
 import frc.utility.Elastic;
 
 public class Robot extends TimedRobot {
-    private final SwerveDrive drive = new SwerveDrive(true);//-10 Works
-    private final Elevator elevator = new Elevator(false);
+    private final SwerveDrive drive = new SwerveDrive(false);//-10 Works
+    private final Elevator elevator = new Elevator(true);
     private final Carriage carriage = new Carriage(
-        new Arm(false),
-        new Pivot(false),
-        new Intake(false) 
+        new Arm(true),
+        new Pivot(true),
+        new Intake(true) 
     );
     
     private Climb climb = new Climb(false);
