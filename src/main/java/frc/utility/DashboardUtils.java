41e8293e0f049fec1ce99d8147cf5ec46fe61b06
ryancave282 <ryancueva282@gmail.com>
2025-08-26 12:15:26 -0500

    add some javadoc to dashboardutils

--- src/main/java/frc/utility/DashboardUtils.java
+++ src/main/java/frc/utility/DashboardUtils.java
@@ -6,18 +6,28 @@ import java.util.List;
 public class DashboardUtils {
     private static final List<Dashboard> publishers = new ArrayList<>();
 
-    // Called by each subsystem in its constructor
+    /**
+     * Call this function to register the subsystem's {@code elasticInit()} method
+     * to be run at robot startup
+     * @param subsystem set to {@code this} while in a subsystem class
+     */
     public static void register(Dashboard subsystem) {
         publishers.add(subsystem);
     }
-
-    // Call this once in Robot.robotInit()
+    
+    /**
+     * Call this once in {@code Robot.robotInit()}
+     */
     public static void initAll() {
         for (Dashboard pub : publishers) {
             pub.elasticInit();
         }
     }
+    
     public interface Dashboard{
+        /**
+         * Place all elastic configs in here to be run at robot startup
+         */
         public void elasticInit();
     }
     
